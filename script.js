c2dd9b28fd26bc478366d10ad9185990d944a466
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2021-02-06 13:59:10 -0300

    new feature, go to top button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const nav = document.querySelector("nav");
 const box = document.querySelector(".box");
-
+const goTop = document.querySelector("#top");
 
 box.addEventListener('click', () => {
     nav.classList.toggle('open');
 });
 
-
-
+window.onscroll = function(){
+    if(window.scrollY > 20){
+        goTop.classList.add("show");
+    } else {
+        goTop.classList.remove("show");
+    }
+}
